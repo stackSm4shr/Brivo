599a6778d68b4730b7f19d11e7e4428ad33cd3bd
Tim <timbecker1988@gmail.com>
2026-03-06 10:47:53 +0100

    Added a basic Dashboard Skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import { ComponentExample } from "@/components/component-example";
+const Homepage = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p4 rounded-lg lg:col-span-2 ">
+        Test
+      </div>
+      <div className="bg-primary-foreground  p4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground  p4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground  p4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground  p4 rounded-lg lg:col-span-2">
+        Test
+      </div>
+      <div className="bg-primary-foreground p4 rounded-lg">Test</div>
+    </div>
+  );
+};
 
-export default function Page() {
-return <ComponentExample />;
-}
+export default Homepage;
